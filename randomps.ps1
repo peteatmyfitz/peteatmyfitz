45453f95fd3f7dc6758b5c852a32c83f87adcd6d
peteatmyfitz <pete@myfitz.net>
2024-12-19 14:31:56 -0500

    Remove option had an issue randomps.ps1

--- randomps.ps1
+++ randomps.ps1
@@ -4,16 +4,3 @@ Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" 
                  @{Name="Publisher";Expression={$_.GetValue("Publisher")}}, 
                  @{Name="InstallDate";Expression={$_.GetValue("InstallDate")}}, 
                  @{Name="Version";Expression={$_.GetValue("DisplayVersion")}} 
-
-# Find the application in the registry
-$app = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | 
-    Get-ItemProperty | 
-    Where-Object {$_.DisplayName -match "YourApplicationName"}
-
-# Check if UninstallString exists
-if ($app.UninstallString) {
-    # Execute the UninstallString
-    & $app.UninstallString
-} else {
-    Write-Warning "UninstallString not found for the application."
-}
